8716cedc21bd6051e785efd0dd3053deb35bafbd
Vytautas Vasiliauskas <vytautas1983@gmail.com>
2020-08-16 19:27:37 +0300

    Serving HTML Pages

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,11 @@
-const express = require('express');
+const path = require("path")
 
-const router = express.Router();
+const express = require("express")
 
-router.get('/', (req, res, next) => {
-  res.send('<h1>Hello from Express!</h1>');
-});
+const router = express.Router()
 
-module.exports = router;
+router.get("/", (req, res, next) => {
+ res.sendFile(path.join(__dirname, "../", "views", "shop.html"))
+})
+
+module.exports = router
